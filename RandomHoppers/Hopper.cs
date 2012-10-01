0edf70e4949412700a9b57ee7c6a5c9346583fa7
Josh Palmer <joshpalmer123@gmail.com>
2012-10-01 19:33:46 +0100

    Added ToCSV method to take any statistic-returning method and make a CSV

--- RandomHoppers/Hopper.cs
+++ RandomHoppers/Hopper.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+
 namespace RandomHoppers
 {
     class Hopper
@@ -16,49 +17,80 @@ namespace RandomHoppers
             return rand.NextDouble();
         }
 
+
+
         static void Main(string[] args)
         {
-            // Create a file to list our number of hops
-            System.IO.StreamWriter file = new System.IO.StreamWriter("d:\\hops.csv");
-            // doing this as a csv so we can have like 10 columns to take statistics on it in excel without overflowing the ~1m row limit in excel (lol)
+            ToCSV(Hop,"Hop",1000);
 
-            // create an array of 10m hop values (it onlt takes 20s so fuck it, why not!)
-            // using doubles since we're going to take an average of it
-
-            int test_runs = 1000;
-
-            double[] hops = new double[test_runs];
-            string line = "";
-            for (int i = 0; i < test_runs; i++)
-            {
-                hops[i] = Hop();
-                if (i % 10 == 0)
-                {
-                    line = line + hops[i].ToString() + ", ";
-                    file.WriteLine(line);
-                    line = "";
-                }
-                else line = line + hops[i].ToString() + ", ";
-                // Hope returns int but it should implicitly cast to double
-            }
-
-            file.Close();
-            // close file
-
-            double average_hops = hops.Average();
+            //double average_hops = hops.Average();
 
             // take average of array
 
-            Console.WriteLine(average_hops);
-                        
+            //Console.WriteLine(average_hops);
+                                 
             return;
         }
 
-        static int Hop()
+        static void ToCSV(Func<double> method, string method_str, int iterations)
+        {
+            // Method to run another method a certain number of times, and spit out a CSV file based on whatever the method returns.
+            // Use it to create statistics on, well, pretty much anything!
+
+            // Takes arguments method (must return a covariant of double. i.e. floats and ints are ok), that method as a string (this is purely for naming files),
+            // and the number of iterations to run it for.
+
+            // CSV file is excel compatible; starts off as a single column file until it hits excel 2013's row limit (~1m)
+            
+            
+            // Create a file to list our number of stat
+            // Doing this as a .csv so statistics in excel are easy to manage
+            
+            string timestamp = DateTime.Now.ToString("yyyy-mm-d_hh-mm-ss");
+            System.IO.StreamWriter file = new System.IO.StreamWriter("D:\\Coding\\PHYS2320_Computing_2\\RandomHoppers\\" + method_str + "_" + iterations + "_" + timestamp + ".csv");
+            
+            int columns;
+
+            if (iterations > 1000000)
+            {
+                columns = 1000000 / iterations;
+            }
+            else columns = 1;
+            // make it fit excel's 1m row limit
+
+            // Create an array for the statistic; using doubles incase we want to take an average of it
+
+            double[] stat = new double[iterations];
+            string line = "";
+            for (int i = 0; i < iterations; i++)
+            {
+                stat[i] = method();
+                // call the method specified when calling this method (ToCSV)
+                if ((i+1) % columns == 0)
+                {
+                    line = line + stat[i].ToString() + ", ";
+                    file.WriteLine(line);
+                    line = "";
+                }
+                else line = line + stat[i].ToString() + ", ";
+                // if method returns int should implicitly cast to double
+            }
+
+            if (line != "")
+                file.WriteLine(line);
+
+            // if it ended on i = 999 and line is not blank or whatever write the residual bit
+            file.Close();
+            // close file
+
+            // TODO: add averages here
+        }
+
+        static double Hop()
         {
             int length = 50; // length of line to hop
             int pos = 1; // position, x 
-            int time = 0; // time counter
+            double time = 0; // time counter
 
             // PrintCurrentState(time, length, pos);
 
@@ -67,10 +99,9 @@ namespace RandomHoppers
                 time++;
                 // start of a "hop", increase time by 1 unit
 
+                // flip coin to see if you hop
                 if (GetNextDouble() > 0.5)
                     pos++;
-
-                // flip coin to see if you hop
 
                 // PrintCurrentState(time, length, pos);
                 // print what it looks like now
@@ -80,8 +111,7 @@ namespace RandomHoppers
                 if (pos == length)
                 {
                     // Console.WriteLine("End of line reached. It took " + time + " hops to reach the end of the line of length " + length);
-                    return time;
-                    // break;
+                    return time; // return number of hops taken to reach end of line
                     // break out here, when the position reaches length
                 }
             }
